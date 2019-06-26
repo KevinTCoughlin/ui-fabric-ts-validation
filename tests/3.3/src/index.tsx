fa0648795f4292ecf4420b77903a4acf32e04736
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-06-26 08:31:05 -0700

    Test Panel example compilation

---
+++ tests/3.3/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { PanelNavigationExample } from "./PanelNavigationExample";
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+export * from "office-ui-fabric-react";
+
+class App extends React.Component {
+    render() {
+        return <PanelNavigationExample />
+    }
+}
